d6af63c1aa3314c22005ecd2a16f12a0da975372
David Jake Morfe <jakemorfe@gmail.com>
2021-06-08 20:18:09 -0400

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name = "PyPlaybook",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "David Morfe",
     author_email = "morphetech@gmail.com",
     license = "MIT",
