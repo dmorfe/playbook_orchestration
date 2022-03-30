1b3fd7fc3840d2418019b4bf866bddc6f66616b7
David Jake Morfe <jakemorfe@gmail.com>
2022-03-29 21:48:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setuptools.setup(
     name = "PyPlaybook",
-    version = "0.0.7",
+    version = "0.0.8",
     author = "David Morfe",
     author_email = "cybercomprepair@gmail.com",
     license = "MIT",
