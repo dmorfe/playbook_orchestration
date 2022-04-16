d54c52ed2d29b802f50396e3a56ba1a39434e117
David Morfe <dmorfe@gmail.com>
2022-04-15 22:18:11 -0400

    updated sample pypi program

--- pyplaybook_tests.py
+++ pyplaybook_tests.py
@@ -1,5 +1,5 @@
 import PyPlaybook
 
-print("Running unit tests...")
-##config = PyPlaybook.Orchestration("Playbook_Template.xlsx")
+print("inputfile", "writeconfig(y/n)", "thread size", "queue side", "delay_factor", "username", "password", "secret","outfolder")
+##config = PyPlaybook.Orchestration("Playbook_Template.xlsx", "y",10,20,90,"username","pass","secret","outfolder")
 ##config.run()
